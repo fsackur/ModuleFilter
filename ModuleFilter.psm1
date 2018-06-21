e570f68573988e67ebb861a846e5a99fd9ae9022
mich8638 <freddie.sackur@rackspace.co.uk>
2018-06-21 14:16:28 +0100

    Initial commit

---
+++ ModuleFilter.psm1
@@ -0,0 +1,19 @@
+﻿
+#Stash the current PSMOdulePath for restoration when user unloads module
+$Script:StashedPSModulePath = $env:PSModulePath
+
+
+#function Select-AutoloadModules
+#$AvailableModules = Get-Module -ListAvailable
+
+#$Selection = $AvailableModules | Out-GridView -Title "Select modules to be available" -OutputMode Multiple
+
+#Code to save selection to local config file
+
+#$env:PSModulePath = ($Selection | select -ExpandProperty ModuleBase) -join [System.IO.Path]::PathSeparator
+
+
+#Unloading the module, or restarting the shell, will remove any filtering.
+$MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
+    $env:PSModulePath = $Script:StashedPSModulePath
+}
