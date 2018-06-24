1da5a835917c90bc2e5129c019d9e4c3805f7853
mich8638 <freddie.sackur@rackspace.co.uk>
2018-06-24 21:48:22 +0100

    Can set filter from parameter

--- Functions/Select-AutoloadModules.ps1
+++ Functions/Select-AutoloadModules.ps1
@@ -7,28 +7,39 @@ function Select-AutoloadModules
     .DESCRIPTION
     Updates the PSModulePath environment variable for the current session to prevent unwanted modules from being autoloaded.
 
-    .PARAMETER Modules
+    .PARAMETER Config
     Modules which should be enabled for autoloading.
+
+    Elements in this collection must have ShouldAutoload, Name and ModuleBase properties.
     #>
     param
     (
         [Parameter(Position = 0)]
-        [psmoduleinfo[]]$Modules
+        [ValidateScript({
+            $_.ShouldAutoload -is [bool] -and
+            $_.Name -is [string] -and -not [string]::IsNullOrWhiteSpace($_.Name) -and
+            $_.ModuleBase -is [string] -and -not [string]::IsNullOrWhiteSpace($_.ModuleBase)
+        })]
+        [psobject[]]$Config
     )
-    $AvailableModules = Get-Module -ListAvailable
 
-    $SelectedModules = $AvailableModules | Out-GridView -Title "Select modules to be enabled for autoloading" -OutputMode Multiple
+    if (-not $Config)   #Execute clause if config is present but empty
+    {
+        $AvailableModules = Get-Module -ListAvailable
 
-    $Config = $AvailableModules | foreach {
-        [PSCustomObject]@{
-            ShouldAutoload = $_ -in $SelectedModules
-            Name = $_.Name
-            ModuleBase = $_.ModuleBase -replace '\\(\d+\.){0,3}\d+$'   #remove version string, if present
+        $SelectedModules = $AvailableModules | Out-GridView -Title "Select modules to be enabled for autoloading" -OutputMode Multiple
+
+        $Config = $AvailableModules | foreach {
+            [PSCustomObject]@{
+                ShouldAutoload = $_ -in $SelectedModules
+                Name = $_.Name
+                ModuleBase = $_.ModuleBase -replace '\\(\d+\.){0,3}\d+$'   #remove version string, if present
+            }
+            Add-Member -InputObject $_ NoteProperty -Name 'ShouldAutoload' -Value ($_ -in $Selection)
         }
-        Add-Member -InputObject $_ NoteProperty -Name 'ShouldAutoload' -Value ($_ -in $Selection)
-    }
 
-    Write-AutoloadSelection $Config
+        Write-AutoloadSelection $Config
+    }
 
     $env:PSModulePath = ($Config | where {$_.ShouldAutoload} | select -ExpandProperty ModuleBase) -join [System.IO.Path]::PathSeparator
 }
