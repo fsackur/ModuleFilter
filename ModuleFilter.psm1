eba492391410b9ded8c461383b3de597bf5e948c
mich8638 <freddie.sackur@rackspace.co.uk>
2018-06-24 21:48:38 +0100

    On load, apply previous filter

--- ModuleFilter.psm1
+++ ModuleFilter.psm1
@@ -17,6 +17,10 @@ if (-not (Test-Path $Script:ConfigFile -PathType Leaf))
 Get-ChildItem $PSScriptRoot\Functions | foreach {. $_.FullName}
 
 
+#On load, apply previous filter
+Select-AutoloadModules (Read-AutoloadSelection)
+
+
 #Unloading the module, or restarting the session, will remove any filtering.
 $MyInvocation.MyCommand.ScriptBlock.Module.OnRemove = {
     $env:PSModulePath = $Script:StashedPSModulePath
