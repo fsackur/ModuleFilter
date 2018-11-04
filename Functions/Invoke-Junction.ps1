9421ef4ec545345deba8217f3ac926f032d82f0a
Freddie Sackur <github@dustyfox.uk>
2018-11-04 20:51:13 +0000

    Invoke-Junction errors on failure

--- Functions/Invoke-Junction.ps1
+++ Functions/Invoke-Junction.ps1
@@ -7,5 +7,27 @@ function Invoke-Junction
         $ModuleBase
     )
 
-    $Result = & (Get-JunctionFilePath) $Junction $ModuleBase
+    $Header = (
+        "Junction v1.06 - Windows junction creator and reparse point viewer",
+        "Copyright (C) 2000-2010 Mark Russinovich",
+        "Sysinternals - www.sysinternals.com"
+    ) -join [Environment]::NewLine
+  
+
+    $SuccessPattern = "Created: .*\r\nTargetted at: .*"
+
+
+    $Result = & (Get-JunctionFilePath) $Junction $ModuleBase | Out-String
+
+    $Message = $Result -replace [regex]::Escape($Header)
+    $Message = $Message.Trim()
+
+    if ($Result -match $SuccessPattern)
+    {
+        Write-Verbose $Message
+    }
+    else
+    {
+        Write-Error $Message
+    }
 }
--- Tests/Invoke-Junction.Tests.ps1
+++ Tests/Invoke-Junction.Tests.ps1
@@ -1,23 +1,58 @@
 $ModuleName = 'Prune'
 $FunctionName = 'Invoke-Junction'
-
+Remove-Module $ModuleName -Force -ErrorAction SilentlyContinue
 $Module = Import-Module $ModuleName -Force -PassThru
+
 
 Describe $FunctionName {
 
     Mock "Get-JunctionFilePath" -ModuleName $ModuleName -MockWith {
-        return "Write-Output"
+        $MockScriptblock = {
+            if (-not $Global:JunctionAlreadyExists)
+            {
+"
+Junction v1.06 - Windows junction creator and reparse point viewer
+Copyright (C) 2000-2010 Mark Russinovich
+Sysinternals - www.sysinternals.com
+
+Created: PATH_TO_JUNCTION
+Targetted at: PATH_TO_MODULE
+"
+            }
+            else
+            {
+"
+Junction v1.06 - Windows junction creator and reparse point viewer
+Copyright (C) 2000-2010 Mark Russinovich
+Sysinternals - www.sysinternals.com
+
+Error creating JUNCTION_FILENAME:
+Cannot create a file when that file already exists.
+"
+            }
+        }
+        return $MockScriptblock
     }
 
-    $Result = & $Module {Invoke-Junction "a" "b"}
+    $Global:JunctionAlreadyExists = $false
+    $TestPath = "TestDrive:\JunctionTest"
+
+    $Result = Invoke-Junction $TestPath C:\Temp -ea Stop
 
     It "Gets the path to junction.exe" {
         Assert-MockCalled -CommandName "Get-JunctionFilePath" -ModuleName $ModuleName -Times 1
-
     }
 
     It "Swallows output from junction.exe" {
-        $Result | Should -Be $null
+        $Result | Should -BeNullOrEmpty
+    }
 
+    It "Errors on junction.exe error" {
+        $Global:JunctionAlreadyExists = $true
+
+        {$Result = Invoke-Junction $TestPath $PSScriptRoot -ErrorAction Stop} |
+            Should -Throw "Cannot create a file when that file already exists."
     }
 }
+
+Remove-Variable JunctionAlreadyExists -Scope Global
